9fcd47bc4c107e1130724a2de3a5eda2fb99da25
Andre Oliveira <andreoliveira.adv@hotmail.com>
2023-08-23 14:14:02 -0400

    Section 2 - 12 routes

--- section2/2-10-expressjs-npm/index.js
+++ section2/2-10-expressjs-npm/index.js
@@ -1,6 +1,23 @@
 const express = require("express"); // importing the express
 const app = express(); // starting the express
 
+// route
+app.get("/", function(request, response)
+{
+    response.send("<h1>Welcome to my website!</h1>");
+}); 
+
+app.get("/blog", function(request, response)
+{
+    response.send("<h1>Welcome to my blog!</h1>")
+});
+
+app.get("/channel/youtube", function(request, response)
+{
+    response.send("<h1>Welcome to my youtube channel!</h1>")
+});
+
+// Starting a server
 app.listen(4000, function(error)
 {
     if(error)
