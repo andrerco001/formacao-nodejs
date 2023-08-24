9aa4619bb88108444ff74af5eed9f33893f07ceb
Andre Oliveira <andreoliveira.adv@hotmail.com>
2023-08-24 17:48:47 -0400

    Section 5 - 30 - Bootstrap Installation

--- section5/questions_and_questions/index.js
+++ section5/questions_and_questions/index.js
@@ -6,30 +6,9 @@ const port = 4000;
 app.set('view engine', 'ejs'); // Express will use EJS with view engine
 app.use(express.static('public')); // static files : img, css, etc
 
-app.get("/:name/:lang", (req, res) => 
+app.get("/", (req, res) => 
 {
-    let name = req.params.name;
-    let lang = req.params.lang;
-    let showMessage = false;
-
-    let products = [
-        {name: "Coca-Cola", price: 2.25},
-        {name: "7Up", price: 2.10},
-        {name: "Crush", price: 1.75},
-        {name: "Redbull", price: 6.50},
-        {name: "Orange juice", price: 3.45},
-        {name: "Chocolate", price: 5.25}
-    ];
-
-    res.render("index", 
-    {
-        name: name,
-        lang: lang,
-        company: "Questions&&Answers Solutions",
-        customers: 8000,
-        msg: showMessage,
-        products: products
-    });
+    res.render("index");
 });
 
 app.listen(port, (error) => 
