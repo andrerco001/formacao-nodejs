3568f3fa0664d96ddc46f5d0cb4c6b761a7b960a
Andre Oliveira <andreoliveira.adv@hotmail.com>
2023-08-24 22:09:04 -0400

    Section 5 - 38 - Capturing data from a form with body-parser

--- section5/questions_and_questions/index.js
+++ section5/questions_and_questions/index.js
@@ -1,11 +1,15 @@
 const { name } = require("ejs");
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const port = 4000;
 
 app.set('view engine', 'ejs'); // Express will use EJS with view engine
 app.use(express.static('public')); // static files : img, css, etc
+app.use(bodyParser.urlencoded({extended:false})); // Get information from the form
+app.use(bodyParser.json()); // Receive form information in JSON format
 
+// Routes
 app.get("/", (req, res) => 
 {
     res.render("index");
@@ -18,9 +22,12 @@ app.get("/questions", (req, res)=>
 
 app.post("/savequestion", (req, res)=> 
 {
-    res.send("Form received");
+    let title = req.body.title;
+    let description = req.body.description;
+    res.send("<p>Form received!<br>Title: " + title + "<br>Description: " + description + "</p>");
 });
 
+// Server launcher
 app.listen(port, (error) => 
 {
     if(error)
