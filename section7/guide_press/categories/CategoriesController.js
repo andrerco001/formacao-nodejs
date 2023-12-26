bd051bd1e2c9e09f05484c52bca3619ac5b65d36
Andre Oliveira <andreoliveira.adv@hotmail.com>
2023-12-25 21:21:31 -0500

    Section 7.80 Category form

--- section7/guide_press/categories/CategoriesController.js
+++ section7/guide_press/categories/CategoriesController.js
@@ -1,12 +1,8 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/categories", (req, res) => {
-    res.send("<h1>Router : Categories</h1>");
-});
-
 router.get("/admin/categories/new", (req, res) => {
-    res.send("<h1>Router : Create a new category</h1>");
+    res.render("admin/categories/new");
 });
 
 module.exports = router;
