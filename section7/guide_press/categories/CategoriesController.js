01e018f60e82f10dcc00672573938a07123f6088
Andre Oliveira <andreoliveira.adv@hotmail.com>
2023-12-27 21:47:04 -0500

    Section 7.85 deleting categories

--- section7/guide_press/categories/CategoriesController.js
+++ section7/guide_press/categories/CategoriesController.js
@@ -28,4 +28,23 @@ router.get("/admin/categories", (req, res) => {
     });
 });
 
+router.post("/categories/delete", (req, res) => {
+    let id = req.body.id;
+    if(id != undefined) {
+        if (!isNaN(id)) { // is a number
+            Category.destroy({
+                where: {
+                    id: id
+                }
+            }).then(() => {
+                res.redirect("/admin/categories");
+            });
+        } else { // is not a number
+            res.redirect("/admin/categories");
+        }
+    } else { // is null
+        res.redirect("/admin/categories");
+    }
+});
+
 module.exports = router;
