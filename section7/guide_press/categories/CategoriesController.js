5d62f1a18d3782509ac1d692152e1399d0f41199
Andre Oliveira <andreoliveira.adv@hotmail.com>
2023-12-27 22:49:36 -0500

    Section 7.87 edit form

--- section7/guide_press/categories/CategoriesController.js
+++ section7/guide_press/categories/CategoriesController.js
@@ -15,7 +15,7 @@ router.post("/categories/save", (req, res) => {
             title: title,
             slug: slugify(title.toLowerCase())
         }).then(()=> {
-            res.redirect("/");
+            res.redirect("/admin/categories");
         });
     } else {
         res.redirect("/admin/categories/new");
@@ -45,6 +45,24 @@ router.post("/categories/delete", (req, res) => {
     } else { // is null
         res.redirect("/admin/categories");
     }
+});
+
+router.get("/admin/categories/edit/:id", (req, res) => {
+    let id = req.params.id;
+
+    if(isNaN(id)) {
+        res.redirect("/admin/categories");
+    }
+    
+    Category.findByPk(id).then(category => {
+        if (category != undefined) {
+            res.render("admin/categories/edit", {category: category});
+        } else {
+            res.redirect("/admin/categories");
+        }
+    }).catch( error => {
+        res.redirect("/admin/categories");
+    });
 });
 
 module.exports = router;
