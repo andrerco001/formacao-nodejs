00c2b226a4ae52d9879bc4671627090d61ace3a3
Andre Oliveira <andreoliveira.adv@hotmail.com>
2023-12-28 22:04:30 -0500

    Section 7.94 saving articles

--- section7/guide_press/articles/ArticlesController.js
+++ section7/guide_press/articles/ArticlesController.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const Category = require("../categories/Category");
+const Article = require("./Article");
+const slugify = require("slugify");
 
 router.get("/articles", (req, res) => {
     res.send("<h1>Router : Articles</h1>");
@@ -9,6 +11,21 @@ router.get("/articles", (req, res) => {
 router.get("/admin/articles/new", (req, res) => {
     Category.findAll().then(categories => {
         res.render("admin/articles/new", {categories: categories});
+    });
+});
+
+router.post("/articles/save", (req, res) => {
+    let title = req.body.title;
+    let body = req.body.body;
+    let category = req.body.category;
+
+    Article.create({
+        title: title,
+        slug: slugify(title.toLowerCase()),
+        body: body,
+        categoryId: category
+    }).then(() => {
+        res.redirect("/articles");
     });
 });
 
