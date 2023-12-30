af6a5c01b28fd528c111c9132732a3716aaae7d8
Andre Oliveira <andreoliveira.adv@hotmail.com>
2023-12-29 23:14:33 -0500

    Section 7.102 article reading page

--- section7/guide_press/index.js
+++ section7/guide_press/index.js
@@ -40,6 +40,23 @@ app.get("/", (req, res) => {
     });
 });
 
+app.get("/:slug", (req, res) => {
+    let slug = req.body.slug;
+    Article.findOne({
+        where: {
+            slug: slug
+        }
+    }).then(article => {
+        if (article != undefined) {
+            res.render("/article", {article: article});
+        } else {
+            res.redirect("/");
+        }
+    }).catch(error => {
+        res.redirect("/");
+    })
+});
+
 // server launch
 app.listen(port, (error) => 
 {
