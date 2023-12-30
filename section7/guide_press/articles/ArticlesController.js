640304c57bc89174446363f8c7d0b78e8bce5a01
Andre Oliveira <andreoliveira.adv@hotmail.com>
2023-12-30 14:07:43 -0500

    Section 7.114 Limiting viewing on the home page

--- section7/guide_press/articles/ArticlesController.js
+++ section7/guide_press/articles/ArticlesController.js
@@ -96,7 +96,10 @@ router.get("/articles/page/:num", (req, res) => {
 
     Article.findAndCountAll({
         limit: 4,
-        offset: offset
+        offset: offset,
+        order: [
+            ['id', 'DESC']
+        ]
     }).then(articles => {
         let next;
         if(offset + 4 >= articles.count) {
@@ -113,8 +116,6 @@ router.get("/articles/page/:num", (req, res) => {
         Category.findAll().then(categories => {
             res.render("admin/articles/page", {result: result, categories: categories});
         });
-    }).catch(error => {
-        res.redirect("");
     });
 });
 
